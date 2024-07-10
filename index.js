178ce48707ab3ab9ee7fceccfbfeea0dd2c14086
bchavs12 <chavesbruno699@gmail.com>
2024-07-10 00:02:58 -0300

    feat: post request w/ middlewares

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
 
 app.listen(
@@ -6,9 +7,24 @@ app.listen(
   () => console.log(`It's Working on port: http://localhost:${PORT}`)
 )
 
+app.use(express.json());
+
 app.get('/tshirt', (req, res) => {
   res.status(200).send({
     tshirt: '👕',
     size: 'large'
+  });
+});
+
+app.post('/tshirt/:id', (req, res) => {
+  const { id } = req.params;
+  const { logo } = req.body;
+
+  if (!logo) {
+    res.status(418).send({ message: 'We need a logo!' })
+  }
+
+  res.send({
+    tshirt: `👕 with your ${logo} and ID of ${id}`
   });
 });
